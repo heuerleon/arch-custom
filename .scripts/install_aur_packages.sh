ad5381feb6bd82f4473a296bbe4edb97340c9c11
Leon Heuer <leon.heuer@outlook.de>
2022-09-03 00:25:26 +0200

    Add AUR packages script, remove arch install script
    Moved arch install script to a new repo

---
+++ .scripts/install_aur_packages.sh
@@ -0,0 +1,7 @@
+# Install yay
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+# Install aur packages
+yay zsh-theme-powerlevel10k-git nerd-fonts-jetbrains-mono slack-desktop lightdm-webkit2-theme-glorious vim-plug jetbrains-toolbox i3lock-color betterlockscreen zoom fastlane
